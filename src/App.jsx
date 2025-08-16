a04d5fb795c7637597d304cd38facada05ddb6cd
SafalBhandari12 <safalbhandari069@gmail.com>
2025-08-16 14:14:36 +0530

    each atoms in atoms family are independent

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,52 @@
-import axios from "axios";
-import { atom, selector } from "recoil";
+import "./App.css";
+import {
+  atomFamily,
+  RecoilRoot,
+  useRecoilState,
+  useRecoilValue,
+  useSetRecoilState,
+} from "recoil";
+import { todosAtomFamily } from "./atoms";
+import { useEffect } from "react";
 
-export const notifications = atom({
-  key: "networkAtom",
-  default: selector({
-    key: "networkAtomSelector",
-    get: async () => {
-      const res = await axios.get("https://nepalak.free.beeceptor.com/get");
-      return res.data;
-    },
-  }),
-});
+function App() {
+  return (
+    <RecoilRoot>
+      <Todo id={1} />
+      <Todo id={2} />
+      <Todo id={2} />
+      <Todo id={2} />
+      <Todo id={2} />
+      <Todo id={2} />
+      <UpdatedTodo />
+    </RecoilRoot>
+  );
+}
 
-export const totalNotificationSelector = selector({
-  key: "totalNotificationSelector",
-  get: ({ get }) => {
-    const allNotifications = get(notifications);
-    return (
-      allNotifications.network +
-      allNotifications.jobs +
-      allNotifications.notifications +
-      allNotifications.messaging
-    );
-  },
-});
+function UpdatedTodo() {
+  const updateTodo = useSetRecoilState(todosAtomFamily(2));
+  useEffect(() => {
+    setTimeout(() => {
+      updateTodo({
+        id: "2",
+        title: "I  can change anything",
+        description: "I am safal",
+      });
+    }, 2000);
+  }, []);
+
+  return <div>Hello</div>;
+}
+
+function Todo({ id }) {
+  const todo = useRecoilValue(todosAtomFamily(id));
+  return (
+    <>
+      {todo.title}
+      {todo.description}
+      <br />
+    </>
+  );
+}
+
+export default App;
