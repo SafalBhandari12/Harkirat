8343df9ac1bf0c9d2ec3b7e267922b25b1af0e33
safalbhandri12 <safalbhandari069@gmail.com>
2025-08-19 20:02:02 +0530

    flex box concepts of css

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,55 @@
-import { RecoilRoot } from "recoil";
-import BackgroundChanger from "./pages/BackgroundChanger";
-import LoginOtp from "./pages/LoginOtp";
-import ParaGenerator from "./pages/ParaGenerator";
-import ProfilePage from "./pages/ProfilePage";
-
 function App() {
   return (
-    <>
-      {/* <ProfilePage /> */}
-      {/* <BackgroundChanger /> */}
-      {/* <ParaGenerator /> */}
-      <RecoilRoot>
-        <LoginOtp />
-      </RecoilRoot>
-    </>
+    <div className='m-4'>
+      <div className='w-4/5 h-[700px] border-[10px] border-[#0f172a] flex justify-around items-center flex-wrap content-start'>
+        <div className='w-[150px] h-[150px] bg-[#fb7185] p-4 font-bold text-[#f1f5f9] text-center border-[10px] border-[#e11d48] rounded-[10px]'>
+          Item 1
+        </div>
+        <div className='w-[150px] h-[150px] bg-[#fb7185] p-4 font-bold text-[#f1f5f9] text-center border-[10px] border-[#e11d48] rounded-[10px]'>
+          Item 2
+        </div>
+        <div className='w-[150px] h-[150px] bg-[#fb7185] p-4 font-bold text-[#f1f5f9] text-center border-[10px] border-[#e11d48] rounded-[10px]'>
+          Item 3
+        </div>
+        <div className='w-[150px] h-[150px] bg-[#fb7185] p-4 font-bold text-[#f1f5f9] text-center border-[10px] border-[#e11d48] rounded-[10px]'>
+          Item 1
+        </div>
+        <div className='w-[150px] h-[150px] bg-[#fb7185] p-4 font-bold text-[#f1f5f9] text-center border-[10px] border-[#e11d48] rounded-[10px]'>
+          Item 2
+        </div>
+        <div className='w-[150px] h-[150px] bg-[#fb7185] p-4 font-bold text-[#f1f5f9] text-center border-[10px] border-[#e11d48] rounded-[10px]'>
+          Item 3
+        </div>
+        <div className='w-[150px] h-[150px] bg-[#fb7185] p-4 font-bold text-[#f1f5f9] text-center border-[10px] border-[#e11d48] rounded-[10px]'>
+          Item 1
+        </div>
+        <div className='w-[150px] h-[150px] bg-[#fb7185] p-4 font-bold text-[#f1f5f9] text-center border-[10px] border-[#e11d48] rounded-[10px]'>
+          Item 2
+        </div>
+        <div className='w-[150px] h-[150px] bg-[#fb7185] p-4 font-bold text-[#f1f5f9] text-center border-[10px] border-[#e11d48] rounded-[10px]'>
+          Item 3
+        </div>
+        <div className='w-[150px] h-[150px] bg-[#fb7185] p-4 font-bold text-[#f1f5f9] text-center border-[10px] border-[#e11d48] rounded-[10px]'>
+          Item 1
+        </div>
+        <div className='w-[150px] h-[150px] bg-[#fb7185] p-4 font-bold text-[#f1f5f9] text-center border-[10px] border-[#e11d48] rounded-[10px]'>
+          Item 2
+        </div>
+        <div className='w-[150px] h-[150px] bg-[#fb7185] p-4 font-bold text-[#f1f5f9] text-center border-[10px] border-[#e11d48] rounded-[10px]'>
+          Item 3
+        </div>
+        <div className='w-[150px] h-[150px] bg-[#fb7185] p-4 font-bold text-[#f1f5f9] text-center border-[10px] border-[#e11d48] rounded-[10px]'>
+          Item 1
+        </div>
+        <div className='w-[150px] h-[150px] bg-[#fb7185] p-4 font-bold text-[#f1f5f9] text-center border-[10px] border-[#e11d48] rounded-[10px]'>
+          Item 2
+        </div>
+        <div className='w-[150px] h-[150px] bg-[#fb7185] p-4 font-bold text-[#f1f5f9] text-center border-[10px] border-[#e11d48] rounded-[10px]'>
+          Item 3
+        </div>
+      </div>
+    </div>
   );
 }
+
 export default App;
