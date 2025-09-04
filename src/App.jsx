4d476252a1a5a867975391807b5fcf5d13fb9c5f
SafalBhandari12 <safalbhandari069@gmail.com>
2025-09-04 19:01:55 +0530

    added getMouseLocationPointer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import React, { useEffect, useState } from "react";
-import useIsOnline from "./hooks/useIsOnline";
+
+function useGetMouseLocation() {
+  const [pointer, setPointer] = useState({ x: 0, y: 0 });
+  const setMousePointer = (value) =>
+    setPointer({ x: value.screenX, y: value.screenY });
+
+  useEffect(() => {
+    window.addEventListener("mousemove", setMousePointer);
+
+    return () => {
+      removeEventListener("mousemove", setMousePointer);
+    };
+  }, []);
+  console.log(pointer);
+
+  return { pointer };
+}
 
 function App() {
-  const { isOnline } = useIsOnline();
+  const { pointer } = useGetMouseLocation();
+  console.log(pointer);
   return (
-    <div>{isOnline ? <p>Hello online user</p> : <p>Hi offline user</p>}</div>
+    <div>
+      <p>X-postion: {pointer.x} </p>
+      <p>Y-position: {pointer.y} </p>
+    </div>
   );
 }
 
