058d4580bcd349b30bed788ab09ed067c52e906c
SafalBhandari12 <safalbhandari069@gmail.com>
2025-09-04 19:46:13 +0530

    added useIntervalHook

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React, { use, useEffect, useState } from "react";
 
-function useGetMouseLocation() {
-  const [pointer, setPointer] = useState({ x: 0, y: 0 });
-  const setMousePointer = (value) =>
-    setPointer({ x: value.screenX, y: value.screenY });
-
+const useInterval = (fun, timeOut) => {
   useEffect(() => {
-    window.addEventListener("mousemove", setMousePointer);
+    const intervalFunction = setInterval(() => {
+      fun();
+    }, timeOut * 1000);
 
     return () => {
-      removeEventListener("mousemove", setMousePointer);
+      clearInterval(intervalFunction);
     };
   }, []);
-  console.log(pointer);
-
-  return { pointer };
-}
+};
 
 function App() {
-  const { pointer } = useGetMouseLocation();
-  console.log(pointer);
-  return (
-    <div>
-      <p>X-postion: {pointer.x} </p>
-      <p>Y-position: {pointer.y} </p>
-    </div>
-  );
+  useInterval(() => {
+    console.log("Hi");
+  }, 5);
+  return <div>App</div>;
 }
 
 export default App;
